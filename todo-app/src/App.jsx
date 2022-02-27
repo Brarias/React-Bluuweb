1c61ebf60ef2c51cae32400bb9268ee8e79c94e9
Brarias <braianrodrigo14@gmail.com>
2022-02-27 12:24:45 -0300

    clase03: Realización de un To-Do List

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ToDoList from "./components/ToDoList";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>App</h1>
+      <ToDoList />
+    </div>
+  );
+};
+
+export default App;
---
+++ todo-app/src/components/Formulario.jsx
@@ -0,0 +1,106 @@
+import Swal from "sweetalert2";
+import { v4 as uuidv4 } from "uuid";
+import { useFormulario } from "../hooks/useFormulario";
+
+const Formulario = ({ agregarTodo }) => {
+  const initialState = {
+    nombre: "",
+    descripcion: "",
+    estado: "pendiente",
+    prioridad: false,
+  };
+
+  const [inputs, handleChange, reset] = useFormulario(initialState);
+
+  const { nombre, descripcion, estado, prioridad } = inputs;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!nombre.trim()) {
+      e.target[0].focus();
+      Swal.fire({
+        title: "Error!",
+        text: "No deje el nombre en blanco",
+        icon: "error",
+      });
+      return;
+    }
+
+    if (!descripcion.trim()) {
+      e.target[1].focus();
+      Swal.fire({
+        title: "Error!",
+        text: "No deje la descripción en blanco",
+        icon: "error",
+      });
+      return;
+    }
+
+    agregarTodo({
+      nombre: nombre,
+      descripcion: descripcion,
+      estado: estado === "pendiente" ? false : true,
+      prioridad: prioridad,
+      id: uuidv4(),
+    });
+
+    Swal.fire({
+      title: "Éxito!",
+      text: "Tarea agregada",
+      icon: "success",
+    });
+
+    reset();
+  };
+
+  return (
+    <>
+      <h3>Agregar TODO</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-control mb-2"
+          name="nombre"
+          placeholder="ingrese todo nombre"
+          value={nombre}
+          onChange={handleChange}
+        />
+        <textarea
+          className="form-control mb-2"
+          placeholder="Ingrese descripcion"
+          name="descripcion"
+          value={descripcion}
+          onChange={handleChange}
+        ></textarea>
+        <select
+          name="estado"
+          className="form-control mb-2"
+          value={estado}
+          onChange={handleChange}
+        >
+          <option value="pendiente">Pendiente</option>
+          <option value="completado">Finalizado</option>
+        </select>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            checked={prioridad}
+            id="flexCheckDefault"
+            name="prioridad"
+            onChange={handleChange}
+          />
+          <label className="form-check-label" htmlFor="flexCheckDefault">
+            Dar prioridad
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Agregar
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default Formulario;
