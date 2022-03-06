fe623d74f34ffe18ce6f6e0e36e3acbfa6f9b378
Brarias <braianrodrigo14@gmail.com>
2022-03-06 09:16:51 -0300

    Se agregó localStorage para mostrar búsquedas anteriores al recargar

--- api-rick/src/App.jsx
+++ api-rick/src/App.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Formulario from "./components/Formulario";
 import PintarDatos from "./components/PintarDatos";
 
 // capturamos el nmombre ingresado en el formulario
 const App = () => {
   const [nombrePersonaje, setNombrePersonaje] = useState(""); // importamos el nombre del personaje desde Formulario. nombrePersonaje viaja a PintarDatos y setNombrePersonaje lo almacena en App
+
+  // usamos localStorage para que muestre los resultados anteriores al recargar la página
+  useEffect(() => {
+    console.log(localStorage.getItem("nombreApi"));
+    if (localStorage.getItem("nombreApi")) {
+      setNombrePersonaje(JSON.parse(localStorage.getItem("nombreApi")));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("nombreApi", JSON.stringify(nombrePersonaje));
+  }, [nombrePersonaje]);
 
   return (
     <div className="container">
